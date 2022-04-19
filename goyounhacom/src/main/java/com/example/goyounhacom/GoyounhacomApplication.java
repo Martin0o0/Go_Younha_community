aa7d2f7155054dd41292e357c1a8c318835e7338
choihyunyeong <8546088@naver.com>
2022-04-19 10:55:29 +0900

    프로젝트2 리파지토리 생성

---
+++ goyounhacom/src/main/java/com/example/goyounhacom/GoyounhacomApplication.java
@@ -0,0 +1,13 @@
+package com.example.goyounhacom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoyounhacomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoyounhacomApplication.class, args);
+    }
+
+}
---
+++ goyounhacom/src/test/java/com/example/goyounhacom/GoyounhacomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goyounhacom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoyounhacomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
